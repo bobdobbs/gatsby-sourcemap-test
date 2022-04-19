80ddbe656bb6de2cb05297eab315c20df1e27386
imacarpet Dobbs <imacarpet@gmx.com>
2022-04-19 16:49:36 +1200

    Initial commit

---
+++ sourcemap-test/src/pages/index.js
@@ -0,0 +1,22 @@
+import * as React from "react"
+import { html, body } from '../components/style.module.scss';
+
+
+// markup
+const IndexPage = () => {
+
+  return (
+    <div>
+
+
+      <h1>Gatsby Sourcemap Test</h1>
+
+      <p>Let's see if we can get sourcemaps working.</p>
+
+    </div>
+
+  )
+
+}
+
+export default IndexPage
